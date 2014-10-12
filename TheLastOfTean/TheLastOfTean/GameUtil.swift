fdfb2e16f205b9bd14193e9f4c2b4169cfe93055
gulingfeng <gulingfeng@gulingfengdeMac.local>
2014-10-11 17:40:28 -0700

    change position to percentage

--- TheLastOfTean/TheLastOfTean/GameUtil.swift
+++ TheLastOfTean/TheLastOfTean/GameUtil.swift
@@ -158,7 +158,12 @@ class GameUtil: NSObject
                     label.selfDelete = resDef.selfDelete
                     label.text = "unknow type"
                 }
-                resource.frame = CGRect(x: detail.resource.positionX, y: detail.resource.positionY, width: detail.resource.width, height: detail.resource.height)
+                var appFrame = UIScreen.mainScreen().applicationFrame
+                var x = CGFloat(appFrame.maxX*CGFloat(detail.resource.positionX/100))
+                var y = CGFloat(appFrame.maxY*CGFloat(detail.resource.positionY/100))
+                var width = CGFloat(appFrame.width*CGFloat(detail.resource.width/100))
+                var height = CGFloat(appFrame.height*CGFloat(detail.resource.height/100))
+                resource.frame = CGRect(x: x, y: y, width: width, height: height)
                 vc.view.addSubview(resource)
             }
         }
