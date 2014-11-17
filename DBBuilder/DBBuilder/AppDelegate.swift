4b3b972b1a9c3378a97b551054b9aaeb84f5cc5d
gulingfeng <gulingfeng@gulingfengdeMac.local>
2014-11-16 17:08:05 -0800

    11-16

--- DBBuilder/DBBuilder/AppDelegate.swift
+++ DBBuilder/DBBuilder/AppDelegate.swift
@@ -8,8 +8,9 @@
 
 import Cocoa
 
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate{ //NSOutlineViewDataSource, NSOutlineViewDelegate {
     
+    var treeNode = NSArray(array: [["FirstColumn":"aaa"],["FirstColumn":"bbb"]])
     
     @IBOutlet weak var window: NSWindow!
 
@@ -20,6 +21,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var selectButton: NSButton!
     
     @IBOutlet weak var tableNameList: NSOutlineView!
+    
+    @IBOutlet weak var tableNameColumn: NSTableColumn!
+    
+
+   
+    @IBOutlet weak var tableNameView: NSTableView!
     
     @IBAction func selectDBFile(sender: NSButton) {
         var fileDialog: NSOpenPanel = NSOpenPanel()
@@ -41,7 +48,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         //DBUtilSingleton.shared.connection?.executeQuery("select * from ", withArgumentsInArray: nil)
     }
-
     
     @IBAction func connectDB(sender: NSButton) {
         if (DBUtilSingleton.shared.dbFilePath != ""){
@@ -55,9 +61,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             
             }
             ///test
-            var tableName = TableNameListTree()
-            tableNameList.setDataSource(tableName)
-
+            //var tableName = TableNameListTree()
+            //tableNameList.setDataSource(tableName)
+            //tableNameList.insertText("aaa")
+            //tableNameList.insertValue("bbb", atIndex: 0, inPropertyWithKey: "b")
+            //treeNode = NSArray(array: ["aaa","bbb"])
+            //tableNameList.setDataSource(self)
+            
+            tableNameView.setDataSource(self)
+            
         }
         else{
             var alertView:NSAlert = NSAlert()
@@ -72,13 +84,53 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(aNotification: NSNotification?) {
         // Insert code here to initialize your application
-    
+        //tableNameList.setDelegate(self)
+        //tableNameList.tableColumns
+   
     }
 
     func applicationWillTerminate(aNotification: NSNotification?) {
         // Insert code here to tear down your application
     }
 
+    /*func outlineView(outlineView: NSOutlineView!, child index: Int, ofItem item: AnyObject!) -> AnyObject!{
+        println(treeNode.objectAtIndex(index))
+        return (item == nil) ? treeNode.objectAtIndex(index) : nil
+    }
+    
+    func outlineView(outlineView: NSOutlineView!, isItemExpandable item: AnyObject!) -> Bool{
+        if(item == nil){
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func outlineView(outlineView: NSOutlineView!, numberOfChildrenOfItem item: AnyObject!) -> Int{
+        println(treeNode.count)
+        return (item == nil) ? treeNode.count : 0
+    }
+    
+    func outlineView(outlineView: NSOutlineView!, objectValueForTableColumn tableColumn: NSTableColumn!, byItem item: AnyObject!) -> AnyObject!{
+        println(tableColumn.identifier)
+        return "tet"
+    }
 
+    func outlineView(outlineView: NSOutlineView!, viewForTableColumn tableColumn: NSTableColumn!, item: AnyObject!) -> NSView!{
+        println("111")
+        return outlineView.makeViewWithIdentifier("column1", owner: self) as NSView
+    }*/
+    
+    func numberOfRowsInTableView(tableView: NSTableView!) -> Int{
+        println(treeNode.count)
+        return treeNode.count
+    }
+    
+    func tableView(tableView: NSTableView!, objectValueForTableColumn tableColumn: NSTableColumn!, row: Int) -> AnyObject!{
+        println(treeNode.objectAtIndex(row).objectForKey(tableColumn.identifier))
+        return treeNode.objectAtIndex(row).objectForKey(tableColumn.identifier)
+        
+    }
 }
+
 
